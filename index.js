c27b24b76044d3a02012dc3912081fe502b66303
Ben Moore <btmoore40@gmail.com>
2019-09-25 23:13:12 -0400

    Removing Hard Coded Username and password

--- index.js
+++ index.js
@@ -13,35 +13,40 @@ app.get("/", (req, res, next) => {
     res.json("Hello World")
  });
 
+const url = 'https://UserAgent:@65a20f83eb0e270dc089e94b37749a277d148275e45e5b41c4442c13.staging-cloud-agents.us-east.containers.appdomain.cloud'
+
 app.get("/agentIdentity", (req, res, next) => {
-    getAgentIdentity(res)
+    const agent_user = req.query.user
+    const agent_password = req.query.password
+    if (typeof agent_user === 'undefined' || typeof agent_password === 'undefined') {
+        res.send("Undefined Username or Password for Agent Identity")    
+    }else{
+        getAgentIdentity(res, agent_user, agent_password)
+    }
  });
 
-
-async function getAgentIdentity(res) {
-    /* User Agent */
+async function getAgentIdentity(res, user, password) {
     const Agent = require('openssi-websdk').Agent;
     const opts = {state: 'inbound_offer'};
-    const user_account_url = 'https://UserAgent:@65a20f83eb0e270dc089e94b37749a277d148275e45e5b41c4442c13.staging-cloud-agents.us-east.containers.appdomain.cloud'
-    const user_agent_name = 'UserAgent';
-    const user_agent_password = '6Fz0R9En20a3D3KDkxVJ';
-    const agent = new Agent(user_account_url, user_agent_name, user_agent_password);
+    const agent = new Agent(url, user, password);
     const agentInfo = await agent.getIdentity();
     res.send(`Agent Identity: ${JSON.stringify(agentInfo, 0, 1)}`)
 }
 
 app.get("/agentCredentials", (req, res, next) => {
-    getAgentCredentials(res)
+    const agent_user = req.query.user
+    const agent_password = req.query.password
+    if (typeof agent_user === 'undefined' || typeof agent_password === 'undefined') {
+        res.send("Undefined Username or Password for Agent Credentials")    
+    }else{
+        getAgentCredentials(res, agent_user, agent_password)
+    }
  });
 
-async function getAgentCredentials(res) {
-    /* User Agent */
+async function getAgentCredentials(res, user, password) {
     const Agent = require('openssi-websdk').Agent;
     const opts = {state: 'inbound_offer'};
-    const user_account_url = 'https://UserAgent:@65a20f83eb0e270dc089e94b37749a277d148275e45e5b41c4442c13.staging-cloud-agents.us-east.containers.appdomain.cloud'
-    const user_agent_name = 'UserAgent';
-    const user_agent_password = '6Fz0R9En20a3D3KDkxVJ';
-    const agent = new Agent(user_account_url, user_agent_name, user_agent_password);
+    const agent = new Agent(url, user, password);
     const credentials = await agent.getCredentials()
     res.send(`Agent Credentials: ${JSON.stringify(credentials, 0, 1)}`)
 }
